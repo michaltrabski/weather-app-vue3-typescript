ebf7a5dbdab6c68c361fc300027ca8a845d823b5
Michal Trabski <instruktormichaltrabski@gmail.com>
2023-09-26 11:25:55 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <Sitenavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import Sitenavigation from '@/components/SiteNavigation.vue';
+</script>
+
+ 
